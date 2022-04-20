be0b050c9c44153090687a964c6084130771194d
PBillingsby <philbillingsby@gmail.com>
2022-04-20 11:53:51 -0700

    center text on root page

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,5 +1,4 @@
-import { Heading, Text } from '@chakra-ui/react';
-
+import { Heading, Text, Center } from '@chakra-ui/react';
 
 export default function Web() {
   return (
@@ -7,10 +6,12 @@ export default function Web() {
       <Heading textAlign="center" fontSize="5xl" fontWeight="bold">
         .devdao domains
       </Heading>
-      <Text>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci et
-        voluptas optio dignissimos facere rem eaque. Cum ea doloribus libero.
-      </Text>
+      <Center>
+        <Text>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci et
+          voluptas optio dignissimos facere rem eaque. Cum ea doloribus libero.
+        </Text>
+      </Center>
     </>
   );
 }
